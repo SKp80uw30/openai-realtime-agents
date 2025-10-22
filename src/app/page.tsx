d762da8a144d5f0b3c65b4f930344cbdbd64fa66
SKp80uw30 <130803257+SKp80uw30@users.noreply.github.com>
2025-10-23 09:25:45 +1100

    Switch Orb into main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import React, { Suspense } from "react";
 import { TranscriptProvider } from "@/app/contexts/TranscriptContext";
 import { EventProvider } from "@/app/contexts/EventContext";
-import App from "./App";
+import OrbShowcase from "./orb-showcase/OrbShowcase";
 
 export default function Page() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <TranscriptProvider>
         <EventProvider>
-          <App />
+          <OrbShowcase />
         </EventProvider>
       </TranscriptProvider>
     </Suspense>
