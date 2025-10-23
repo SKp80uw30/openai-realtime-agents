d54ded5ae0ac50d135d6c9e891b6068bb426a9f8
SKp80uw30 <130803257+SKp80uw30@users.noreply.github.com>
2025-10-23 21:57:46 +1100

    BugFix

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -15,7 +15,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: '/openai-logomark.svg',
         sizes: 'any',
         type: 'image/svg+xml',
-        purpose: 'any maskable',
+        purpose: 'maskable',
       },
       {
         src: '/favicon.ico',
